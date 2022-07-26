68e0421b346472499db2150c65d5e66fe4b9c4da
sandeepnewa10 <sandeepnewa@gmail.com>
2022-07-27 01:22:12 +1000

    adjust in cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,50 +18,16 @@ function App() {
   return (
     <div className="App">
       <>
-
-
-      <BrowserRouter>
-    <Routes>
-  
-        <Route path="/" exact element={<Home />} />
-        <Route path="/product/:id" exact element={<Product />} />
-        <Route path="/products/:category" exact element={ <ProductList />} />
-        <Route path="/cart" exact element={ <Cart />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/register" exact element={ <Register />} />
-       
-      
-    </Routes>
-  </BrowserRouter>
-
-
-
-{/* 
-
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/product/:id">
-              <Product />
-            </Route>
-            <Route path="/products/:category">
-              <ProductList />
-            </Route>
-            <Route path="/cart">
-              <Cart />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-          </Switch>
-        </Router> */}
-
-
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/product/:id" exact element={<Product />} />
+            <Route path="/products/:category" exact element={<ProductList />} />
+            <Route path="/cart" exact element={<Cart />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/register" exact element={<Register />} />
+          </Routes>
+        </BrowserRouter>
       </>
     </div>
   );
